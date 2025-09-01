a6a99455b2c893e01ad9e08d776537b624ef20f9
taf <florian-tauber@gmx.de>
2025-09-01 22:05:40 +0200

    echo back the args given

--- src/main/java/com/example/demo_for_spring_boot_and_jenkins/DemoForSpringBootAndJenkinsApplication.java
+++ src/main/java/com/example/demo_for_spring_boot_and_jenkins/DemoForSpringBootAndJenkinsApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo_for_spring_boot_and_jenkins;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoForSpringBootAndJenkinsApplication {
@@ -10,4 +12,13 @@ public class DemoForSpringBootAndJenkinsApplication {
 		SpringApplication.run(DemoForSpringBootAndJenkinsApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Command-line arguments received: " + args.length);
+			for (String arg : args) {
+				System.out.println(arg);
+			}
+		};
+	}
 }
