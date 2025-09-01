be928f3e72df6787967b48d2086836d92f6fa229
taf <florian-tauber@gmx.de>
2025-09-01 15:30:38 +0200

    first commit

---
+++ src/main/java/com/example/demo_for_spring_boot_and_jenkins/DemoForSpringBootAndJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_for_spring_boot_and_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForSpringBootAndJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForSpringBootAndJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_for_spring_boot_and_jenkins/DemoForSpringBootAndJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_for_spring_boot_and_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForSpringBootAndJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
